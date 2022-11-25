b83ed5ca755b892a09e2f06a045ef3fadc52ad27
Eric Dasse <christ.gououi@gmail.com*>
2022-11-25 06:46:10 +0100

    Use enum to represent cardinal points

--- mars_rover.py
+++ mars_rover.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from enum import Enum
 
 
 @dataclass
@@ -7,13 +8,18 @@ class Point:
     y: float
 
 
+class CardinalPoint(Enum):
+    N = "N"
+    S = "S"
+
+
 class Rover:
     def __init__(self, position, direction):
         self.position = position
         self.direction = direction
 
     def move_forward(self):
-        if self.direction == "S":
+        if self.direction == CardinalPoint.S:
             self.position.y = -1
         else:
             self.position.y += 1
--- test_mars_rover.py
+++ test_mars_rover.py
@@ -1,22 +1,22 @@
 import pytest
-from mars_rover import Point, Rover
+from mars_rover import CardinalPoint, Point, Rover
 
 
 @pytest.mark.parametrize("x,y", [(0, 0), (0, 1), (3, 2), (6, 4)])
 def test_rover_can_move_forward_when_facing_north_direction(x, y):
-    rover = Rover(position=Point(x, y), direction="N")
+    rover = Rover(position=Point(x, y), direction=CardinalPoint.N)
 
     rover.move_forward()
 
-    assert_rover_state(rover, x, y + 1, "N")
+    assert_rover_state(rover, x, y + 1, CardinalPoint.N)
 
 
 def test_rover_can_move_forward_when_facing_south_direction():
-    rover = Rover(position=Point(0, 0), direction="S")
+    rover = Rover(position=Point(0, 0), direction=CardinalPoint.S)
 
     rover.move_forward()
 
-    assert_rover_state(rover, 0, -1, "S")
+    assert_rover_state(rover, 0, -1, CardinalPoint.S)
 
 
 def assert_rover_state(rover, x, y, direction):
