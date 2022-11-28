8673f4c967be270014548c08a36f6694985422f9
Eric Dasse <christ.gououi@gmail.com>
2022-11-28 13:31:04 +0100

    Add messages for rover state assertions

--- tests/helpers.py
+++ tests/helpers.py
@@ -5,6 +5,8 @@ from mars_rover import Rover
 def assert_rover_state(
     rover: Rover, x: float, y: float, faced_direction: CardinalPoint
 ):
-    assert rover.position.x == x
-    assert rover.position.y == y
-    assert rover.faced_direction == faced_direction
+    assert rover.position.x == x, f"Expected x:{x}, got {rover.position.x}"
+    assert rover.position.y == y, f"Expected y:{y} on y, got {rover.position.y}"
+    assert (
+        rover.faced_direction == faced_direction
+    ), f"Expected direction:{faced_direction}, got {rover.faced_direction}"
